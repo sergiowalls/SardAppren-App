b34e4bcc67e04d879e82f0eec84f8caf90bb450e
Gigi <giselaruzafamado@gmail.com>
2019-05-13 01:19:26 +0200

    arreglo sergio

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
 
 import HomeScreen from "./src/screens/HomeScreen";
 import BeatScreen from "./src/screens/BeatScreen";
-import Profile from "./src/screens/UserProfile/Profile";
+import ProfileScreen from "./src/screens/ProfileScreen";
 import RankingScreen from "./src/screens/RankingScreen";
 
 const MainNavigator = createBottomTabNavigator({
-    Profile: {screen: Profile2},
+    Profile: {screen: ProfileScreen},
     Home: {screen: HomeScreen},
     Beat: {screen: BeatScreen},
     Ranking: {screen: RankingScreen},
--- src/screens/RankingScreen.js
+++ src/screens/RankingScreen.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class RankingScreen extends Component {
     static navigationOptions = {
-        title: 'ranking',
-        tabBarIcon: () => (<Icon name="bar-graph" size={30}/>)
+        title: 'Ranking',
+        tabBarIcon: () => (<Icon name="music" size={30}/>)
     };
 
     render() {
         return (
-            <View> </View>
+            <View>
+                <Text>Benvingut</Text>
+            </View>
         );
     }
 
