89210671aab3c9a86b8660068cbc1c420cab07aa
Sergio <sergio.paredes@est.fib.upc.edu>
2019-05-29 21:54:08 +0200

    Move play buttons to modal. Translate play navtab title to catalan.

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -1,79 +1,103 @@
 import React, {Component} from 'react';
-import {Text, View, FlatList, Button} from 'react-native';
+import {Text, View, FlatList, Button, TouchableWithoutFeedback} from 'react-native';
+import Modal from 'react-native-modal';
 import Icon from 'react-native-vector-icons/Entypo';
 
 export default class PlayScreen extends Component {
     static navigationOptions = {
-        title: 'Play',
+        title: 'Jugar',
         tabBarIcon: () => (<Icon name="game-controller" size={30}/>)
     };
 
     constructor(props: P, context: any) {
         super(props, context);
         this.state = {
-            sardanasList: []
+            sardanasList: [],
+            modalVisible: false
         };
 
-        this.renderItem = this.renderItem.bind(this)
+        this.renderItem = this.renderItem.bind(this);
+        this.setModalVisible = this.setModalVisible.bind(this);
+        this.setModalInvisible = this.setModalInvisible.bind(this)
     }
 
     componentWillMount() {
         let list = [
-            {title: 'sardana1'},
-            {title: 'sardana2'},
-            {title: 'sardana3'},
-            {title: 'sardana4'},
-            {title: 'sardana5'},
-            {title: 'sardana6'}
-        ]
+            {title: 'Sardana 1'},
+            {title: 'Sardana 2'},
+            {title: 'Sardana 3'},
+            {title: 'Sardana 4'},
+            {title: 'Sardana 5'},
+            {title: 'Sardana 6'}
+        ];
         setTimeout(() => {
             this.setState({sardanasList: list})
-        }, 2000)
+        }, 1500)
         // fetch('url').then((data)=>{this.setState((sardanasList:data.list))})
     }
 
     renderItem({item}) {
-        const { navigation } = this.props
         return (
             <View>
-                <View style={{
+                <TouchableWithoutFeedback onPress={() => {
+                    this.setModalVisible()
+                }} style={{
                     flexDirection: 'row',
                     paddingTop: 10,
                     paddingBottom: 10,
                     paddingLeft: 15,
                     paddingRight: 15
                 }}>
-                    <View style={{justifyContent: 'center', flex: 1}}><Text
-                        style={styles.item}>{item.title}</Text></View>
-                    <View style={{padding: 5, alignItems: 'center', justifyContent: 'center'}}><Button title="contar"/></View>
-
-                    <View style={{padding: 5, alignItems: 'center', justifyContent: 'center'}}>
-                        <Button title="repartir" onPress={()=>navigation.navigate('BeatScreen')}/>
+                    <View style={{justifyContent: 'center', flex: 1}}>
+                        <Text style={styles.item}>{item.title}</Text>
                     </View>
+                </TouchableWithoutFeedback>
 
-
-                </View>
-                <View style={{height:1, backgroundColor:'rgba(0,0,0,0.2)', marginRight:10, marginLeft:10}}/>
+                <View style={{height: 1, backgroundColor: 'rgba(0,0,0,0.2)', marginRight: 10, marginLeft: 10}}/>
             </View>
-
-
         )
     }
 
     render() {
+        const {navigation} = this.props;
         return (
             <View style={styles.container}>
+                <Text style={styles.title}>Jugar</Text>
                 <FlatList
                     data={this.state.sardanasList}
+                    extraData={this.state}
                     renderItem={this.renderItem}
                     keyExtractor={(item, index) => `id_${index}`}
                 />
+                <Modal isVisible={this.state.modalVisible} onBackdropPress={() => this.setModalInvisible()}
+                       style={{backgroundColor: '#FFF'}}>
+                    <View style={{padding: 5, alignItems: 'center', justifyContent: 'center'}}>
+                        <Button title="Tutorial comptar" onPress={() => {
+                            this.setModalInvisible();
+                            navigation.navigate('BeatScreen')
+                        }}/>
+                    </View>
+                    <View style={{padding: 5, alignItems: 'center', justifyContent: 'center'}}>
+                        <Button title="Comptar" onPress={() => {
+                            this.setModalInvisible();
+                            navigation.navigate('BeatScreen')
+                        }}/>
+                    </View>
+                    <View style={{padding: 5, alignItems: 'center', justifyContent: 'center'}}>
+                        <Button title="Repartir"/>
+                    </View>
+                </Modal>
             </View>
-
-
         );
     }
 
+    setModalVisible() {
+        this.setState({modalVisible: true})
+    }
+
+    setModalInvisible() {
+        this.setState({modalVisible: false})
+    }
 }
 const styles = {
     container: {
@@ -84,5 +108,8 @@ const styles = {
         fontSize: 18,
         height: 44,
     },
-}
-
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+};
