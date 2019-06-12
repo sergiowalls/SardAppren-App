7f568848679732de5ad2e59c6db73227b9dbbb21
Mario Albo <mario.albo@fib.upc.edu>
2019-06-12 21:27:52 +0200

    Add distribute functionality

--- src/screens/DistributeScreen.js
+++ src/screens/DistributeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import BeatScreen from "./BeatScreen";
 
 export default class DistributeScreen extends BeatScreen {
@@ -7,104 +7,108 @@ export default class DistributeScreen extends BeatScreen {
 
     constructor(props: P, context: *) {
         super(props, context);
+        this.distribute = this.distribute.bind(this);
     }
 
     render() {
         const currentTimeString = this.getAudioTimeString(this.state.playSeconds);
 
-        return (
-            <View >
+        if (this.state.fail)
+            return (
+                <View style={styles.containerSuccess}>
+                    <Text style={styles.textStyle}>¡Has fallat!</Text>
+                    <Text style={styles.textStyle}>😡</Text>
+                </View>);
 
+        return (
+            <View>
                 <View style={styles.container}>
-                    <Text style={styles.textStyle} >{currentTimeString}</Text>
-                <Text style={styles.textStyle}  >Curts</Text>
+                    <Text style={styles.textStyle}>{currentTimeString}</Text>
+                    <Text style={styles.textStyle}>Curts</Text>
                 </View>
-                    <View style={{ flexDirection: 'row',  justifyContent: 'center'}}>
-                    <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItCurts}>
+                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                    <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('2')}>
                         <Text>2</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItCurts}>
+                    <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('3')}>
                         <Text>3</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.container}>
-                <Text style={styles.textStyle}  >Llargs</Text>
+                    <Text style={styles.textStyle}>Llargs</Text>
                 </View>
-                <View   style={{ flexDirection: 'row',  justifyContent: 'center'}} >
+                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
 
-                        <View  style={{  flexDirection: 'column', justifyContent: 'space-between'}} >
-                            <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItCurts}>
-                                <Text>2</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItLlargs}>
-                                <Text>3</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItCurts}>
-                                <Text>4</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItLlargs}>
-                                <Text>2+2+2+3</Text>
-                            </TouchableOpacity>
-                        </View>
-
-
-                    <View  style={{ flexDirection: 'column',  alignItems: 'stretch', justifyContent: 'center'}} >
-                        <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItCurts}>
-                        <Text>2+2+2</Text>
+                    <View style={{flexDirection: 'column', justifyContent: 'space-between'}}>
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('2')}>
+                            <Text>2</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItLlargs}>
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('3')}>
+                            <Text>3</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('4')}>
+                            <Text>4</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('2+2+2+3')}>
+                            <Text>2+2+2+3</Text>
+                        </TouchableOpacity>
+                    </View>
+
+
+                    <View style={{flexDirection: 'column', alignItems: 'stretch', justifyContent: 'center'}}>
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('2+2+2')}>
+                            <Text>2+2+2</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('2+2+3')}>
                             <Text>2+2+3</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItCurts}>
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('2+2')}>
                             <Text>2+2</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity style = { styles.buttonView}   onPress={this.beatItLlargs}>
+                        <TouchableOpacity style={styles.buttonView} onPress={() => this.distribute('2+3')}>
                             <Text>2+3</Text>
                         </TouchableOpacity>
 
                     </View>
-            </View>
+                </View>
             </View>
 
 
         );
     }
 
-    countBeats(seconds) {
-        const startSecond = seconds - 5.219;
-        let beatsPerSecond = 1.12724;
-        const beats = startSecond > 0 ? Math.trunc(startSecond / beatsPerSecond) + 1 : 0;
-        this.setState({beats})
+    distribute(propose) {
+        this.setState({fail: true});
+        setTimeout(() => this.setState({fail: false}), 800);
     }
 
 }
 
 const styles = StyleSheet.create({
     container: {
-
         alignItems: 'center',
-        justifyContent:'center',
+        justifyContent: 'center'
+    },
+    containerSuccess: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%'
     },
     buttonView: {
-
         alignItems: 'center',
-        justifyContent:'center',
-        backgroundColor: '#DDDDDD',
-        width:100,
-        height:100,
-        margin:10
-
-    }
-
-    ,
-    textStyle: {fontSize: 20,
-
+        justifyContent: 'center',
+        backgroundColor: '#2097f1',
+        width: 100,
+        height: 100,
+        margin: 10
     },
-
+    textStyle: {
+        fontSize: 20,
+    },
 });
